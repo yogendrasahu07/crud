63f9a3456e0743e756cd54a3b61d27fee0a1ebd5
yogendrasahu07 <yogendrasahu9336@gmail.com>
2025-01-18 11:43:29 +0530

    update

--- src/app/new-cp/new-cp.component.ts
+++ src/app/new-cp/new-cp.component.ts
@@ -13,7 +13,8 @@ export class NewCpComponent  {
   mobile:any
   address:any
   submittedData:any[]=[]
-
+  updateEmail:any
+  isUpdate:boolean=false
   ngOninit(){
 
   }
@@ -33,12 +34,34 @@ export class NewCpComponent  {
 
   }
 
- editDetails(data:any){
-  
-
+ editDetails(email:any){
+  let result=this.submittedData?.filter((item)=>item.email==email);
+  console.log(this.submittedData)
+  this.isUpdate=true;
+  this.name=result[0]?.name;
+  this.email=result[0]?.email;
+  this.mobile=result[0]?.mobile;
+  this.address=result[0]?.address;
   }
 
+
+  update(){
+    const payload={
+      'name':this.name,
+      'email':this.email,
+      'mobile':this.mobile,
+      'address':this.address,
+    }
+
+    let indexId=this.submittedData?.findIndex((item)=>item.email==this.email);
+    this.submittedData[indexId] = payload;
+    this.isUpdate=false
+    this.name=''
+    this.email=''
+    this.mobile=''
+    this.address=''
+  }
   deleteDetails(id:any){
-this.submittedData.splice(id,1)
+    this.submittedData.splice(id,1);
   }
  }
